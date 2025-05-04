98d7cabb2449368c079ff72d63bcbb507536820f
ElMiho <mikael.hjermitslev@gmail.com>
2025-05-04 11:41:45 +0200

    Added task_6.sh
    And fix typo in task5.sh

--- tasks/task6/task_6.sh
+++ tasks/task6/task_6.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#BSUB -J task5
+#BSUB -J task6
 #BSUB -q hpc
 #BSUB -W 180
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "rusage[mem=2GB]"
 #BSUB -R "span[hosts=1]"
 #BSUB -n 32
-#BSUB -o task5_%J.out
-#BSUB -e task5_%J.err
+#BSUB -o task6_%J.out
+#BSUB -e task6_%J.err
 #BSUB -u s214753@dtu.dk
 #BSUB -B
 #BSUB -N
@@ -14,4 +14,5 @@
 
 source /dtu/projects/02613_2024/conda/conda_init.sh
 conda activate 02613
-time python simulate_5.py 1
+
+python simulate_6.py 100
