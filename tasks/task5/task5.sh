40196c6e4efed6c583403f44b76dbfab5771e326
ElMiho <mikael.hjermitslev@gmail.com>
2025-05-04 11:37:15 +0200

    Fix task5.sh

--- tasks/task5/task5.sh
+++ tasks/task5/task5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#BSUB -J task5[1,2,3,4,5,8,10,12,14,16,18,20,22,24,26,28,30,32]
+#BSUB -J task5
 #BSUB -q hpc
-#BSUB -W 10
+#BSUB -W 180
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -R "span[hosts=1]"
-#BSUB -n 1
+#BSUB -n 32
 #BSUB -o task5_%J_%I.out
 #BSUB -e task5_%J_%I.err
 #BSUB -u s214753@dtu.dk
